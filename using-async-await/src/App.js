9b4679945c76b925141bc1d81b4a130d0e7bdd8d
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-07-08 16:30:57 +0000

    cleanup

--- using-async-await/src/App.js
+++ using-async-await/src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    axios("https://teaminnovation-endpoint.herokuapp.com/eoi-list/")
+      .then((res) => {
+        setData(res.data);
+      })
+      // if there's an error, set the error to the error
+      .catch((error) => {
+        console.log("Error fetching Data: ", error);
+        setError(error);
+      })
+      // after the fetch, set loading to false
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>Error: {error.statusText}</div>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      {data.map((dataItem) => (
+        <p key={dataItem.id}>{dataItem.fullname}</p>
+      ))}
     </div>
   );
 }
