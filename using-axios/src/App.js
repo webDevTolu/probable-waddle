32e929281bba9b9ec9b49a4776125dd12b9cdb27
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-07-08 16:26:34 +0000

    cleanup and installed axios🎍

--- using-axios/src/App.js
+++ using-axios/src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("https://teaminnovation-endpoint.herokuapp.com/eoi-list/")
+      // checks if the response is ok ✔
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        // else show the response
+        throw res;
+      })
+      // set the res.json to data
+      .then((data) => {
+        setData(data);
+      })
+      // if there's an error, set the error to the error
+      .catch((error) => {
+        console.log("Error fetching Data: ", error);
+        setError(error);
+      })
+      // after the fetch, set loading to false
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>Error: {error.statusText}</div>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      {data.map((dataItem) => (
+        <p key={dataItem.id}>{dataItem.fullname}</p>
+      ))}
     </div>
   );
 }
